8de4e0675067be91ed6b8dd9203d9ab99738029e
Aiden Chang <aidenchang@pcpartner.com>
2023-05-07 08:45:43 +0800

    Add data code

---
+++ app/src/main/java/com/example/learn_spotify_mvvm_clone/SpotifyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learn_spotify_mvvm_clone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AidenChang 2023/05/06
+ */
+
+@HiltAndroidApp
+class SpotifyApplication : Application()
---
+++ app/src/main/java/com/example/learn_spotify_mvvm_clone/data/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.example.learn_spotify_mvvm_clone.data.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.learn_spotify_mvvm_clone.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Created by AidenChang 2023/05/06
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).applyDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
